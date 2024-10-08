3bbc4327d3987350afe9ab679dc153530e4754e2
Kunal Kishor <kishorkunal382@gmail.com>
2024-10-08 21:13:37 +0530

    Enum

--- harsh.cpp
+++ harsh.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 
-struct Student {
-    int id;
-    string name;
-    float percent;
+enum Days {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
 };
 
-int main () {
-    
+int main () {  
+    enum Days today;
+    today = Sunday;
+    switch (today) {
+        case 0:
+        cout << "Monday";
+        break;
+        case 1:
+        cout << "Tuesday";
+        break;
+        case 2: 
+        cout << "Wednesday";
+        break;
+        default: 
+        cout << "Invalid";
+        break;
+    }
     return 0;
 }
